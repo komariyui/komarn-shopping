64c8ca854965ce96efeca834ff86cff2d0ffd639
shiratama <lightsnow4958@gmail.com>
2022-07-06 14:25:39 +0800

    初始化提交推送

---
+++ src/main/java/com/eShop/shiratama/ShiratamaApplication.java
@@ -0,0 +1,13 @@
+package com.eShop.shiratama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiratamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiratamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eShop/shiratama/ShiratamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eShop.shiratama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiratamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
