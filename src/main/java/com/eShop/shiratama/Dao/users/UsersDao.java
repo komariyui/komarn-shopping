88472a57529e03e4423bf1dae4dc1be54aebb5b8
shiratama <lightsnow4958@gmail.com>
2022-07-28 11:44:50 +0800

    fix: 目录调整

--- src/main/java/com/eShop/shiratama/Dao/users/UsersDao.java
+++ src/main/java/com/eShop/shiratama/Dao/users/UsersDao.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UsersDao {
-//    添加一个用户
+//    添加一个用户/
     public int insertUser(@Param("username") String username,@Param("password") String password);
 //    查询一个用户
     public UsersBean selectUser(@Param("username") String username);
