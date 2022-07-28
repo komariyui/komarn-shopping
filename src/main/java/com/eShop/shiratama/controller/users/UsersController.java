df049cae400cf9a3105be52cc58d0f8dee6efc87
shiratama <lightsnow4958@gmail.com>
2022-07-28 12:32:50 +0800

    feat: 获取验证码添加

--- src/main/java/com/eShop/shiratama/controller/users/UsersController.java
+++ src/main/java/com/eShop/shiratama/controller/users/UsersController.java
@@ -1,21 +1,26 @@
 package com.eShop.shiratama.controller.users;
 
+import com.eShop.shiratama.entity.returnJson.template;
 import com.eShop.shiratama.service.users.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
 @RestController
+@RequestMapping("/users")
 public class UsersController {
 
     @Autowired
     private UsersService usersService;
 
-    @RequestMapping("/register")
-    public HashMap register (@RequestParam String username,@RequestParam String password){
-        return usersService.insertUser(username,password);
+    @PostMapping
+    public HashMap register (@RequestParam String username,@RequestParam String password,@RequestParam String verificationCode){
+        return usersService.insertUser(username,password,verificationCode);
     }
+
+    // get verification code
+    @GetMapping
+    public template getVerificationCode(){return usersService.verificationCode();}
+
 }
