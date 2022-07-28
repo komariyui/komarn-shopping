6da85093d5e5ae8d98ca10d6584767891075a9ea
shiratama <lightsnow4958@gmail.com>
2022-07-28 11:33:40 +0800

    fix: test

--- src/main/java/com/eShop/shiratama/controller/test.java
+++ src/main/java/com/eShop/shiratama/controller/test.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-
+//
 @RestController
 public class test {
     @GetMapping("/test")
