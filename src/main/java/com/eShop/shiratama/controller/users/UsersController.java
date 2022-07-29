c6bad44e8f064f3488ad26b7cce4ba474e445378
shiratama <lightsnow4958@gmail.com>
2022-07-30 00:51:50 +0800

    fix: 后更改

--- src/main/java/com/eShop/shiratama/controller/users/UsersController.java
+++ src/main/java/com/eShop/shiratama/controller/users/UsersController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/users")//
 public class UsersController {
 
     @Autowired
