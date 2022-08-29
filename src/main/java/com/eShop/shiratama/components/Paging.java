e62e90df364090cc2e567cb5ff924eb39ad0eb45
shiratama <lightsnow4958@gmail.com>
2022-08-29 09:37:48 +0800

    feat: 加了一个分页的组件

---
+++ src/main/java/com/eShop/shiratama/components/Paging.java
@@ -0,0 +1,7 @@
+package com.eShop.shiratama.components;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Paging {
+}
--- src/main/java/com/eShop/shiratama/controller/commodity/commodityController.java
+++ src/main/java/com/eShop/shiratama/controller/commodity/commodityController.java
@@ -1,4 +1,9 @@
 package com.eShop.shiratama.controller.commodity;
 
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class commodityController {
+    //Get all commodity(have paging)
 }
