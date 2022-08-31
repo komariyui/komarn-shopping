e35052b2de0ab0de7d7e424a372dbe7867347239
shiratama <lightsnow4958@gmail.com>
2022-08-31 17:14:35 +0800

    feat:摸鱼ing... 创建了收藏的控制器

---
+++ src/main/java/com/eShop/shiratama/controller/users/CollectionController.java
@@ -0,0 +1,10 @@
+package com.eShop.shiratama.controller.users;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/collection")
+public class CollectionController {
+}
